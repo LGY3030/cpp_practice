bef41b81c50265acf83be58a7533af5f34e6abc5
LGY3030 <lgy3030@gmail.com>
2020-12-08 10:47:23 +0800

    basic linked list using struct

--- DynamicProgramming/FibonacciNumbers.cpp
+++ DynamicProgramming/FibonacciNumbers.cpp
@@ -9,9 +9,11 @@ int Fib_dyna(int num);
 
 int main(){
 	for(int i=0; i<1000; i++) arr[i] = 0;
-	int firstnum, secondnum;
+	int firstnum, secondnum, thirdnum, fournum;
 	int answer_first = 0;
 	int answer_second = 0;
+	int answer_third = 0;
+	int answer_four = 0;
 	clock_t start, end;
 	cout<<"Enter a number:";
 	cin>>firstnum;
@@ -25,11 +27,48 @@ int main(){
 	cout<<"Enter a number:";
 	cin>>secondnum;
 	
+	//DP using recursive
 	start = clock();
 	answer_second = Fib_dyna(secondnum);
 	cout<<"Second answer: "<<answer_second<<endl;
 	end = clock();
 	cout<<"Time: "<<double(end-start)/CLOCKS_PER_SEC<<endl<<"-------------------"<<endl;
+	
+	
+	cout<<"Enter a number:";
+	cin>>thirdnum;
+	
+	//DP using iteration
+	start = clock();
+	int *a = new int[thirdnum+1]();
+	a[0] = 0;
+	a[1] = 1;
+	for(int i=2;i<=thirdnum;i++){
+		a[i] = a[i-1] + a[i-2];
+	}
+	answer_third = a[thirdnum];
+	cout<<"Third answer: "<<answer_third<<endl;
+	end = clock();
+	cout<<"Time: "<<double(end-start)/CLOCKS_PER_SEC<<endl<<"-------------------"<<endl;
+	
+	
+	cout<<"Enter a number:";
+	cin>>fournum;
+	//DP using iteration with constant space complexity
+	start = clock();
+	int first = 0;
+	int second = 1;
+	int answer = 0;
+	for(int i=2;i<=fournum;i++){
+		answer = first + second;
+		first = second;
+		second = answer;
+	}
+	answer_four = answer;
+	cout<<"Four answer: "<<answer_four<<endl;
+	end = clock();
+	cout<<"Time: "<<double(end-start)/CLOCKS_PER_SEC<<endl<<"-------------------"<<endl;
+	
 }
 
 int Fib_recur(int num){
---
+++ LinkedList/LinkedList_Basic_Struct.cpp
@@ -0,0 +1,148 @@
+#include<iostream>
+
+using namespace std;
+
+struct Node{
+	int data;
+	Node * next;
+};
+
+void push_front(int, Node*&);
+void push_end(int, Node*&);
+void pop_front(Node*&);
+void pop_end(Node*&);
+void pop_index(int, Node*&);
+void pop_num(int, Node*&);
+void search(int, Node*&);
+void print_list(Node*);
+ 
+int main(){
+	Node * head = NULL;
+	push_front(5, head);
+	push_front(10,head);
+	push_end(2,head);
+	push_end(15,head);
+	search(1,head);
+	print_list(head);
+} 
+
+void push_front(int num, Node*& head){
+	Node * current = new Node;
+	current->data = num;
+	current->next = head;
+	head = current;
+}
+
+void push_end(int num, Node*& head){
+	Node * current = new Node;
+	current->data = num;
+	current->next = NULL;
+	Node * temp = head;
+	if(head == NULL) head = current;
+	else{
+		while(temp->next!=NULL) temp=temp->next;
+		temp->next = current;
+	}
+	
+}
+
+void pop_front(Node*& head){
+	if(head == NULL){
+		cout<<"The list is empty"<<endl;
+	}
+	else{
+		Node * tmp = head;
+		head = head->next;
+	}
+}
+
+void pop_end(Node*& head){
+	if(head == NULL){
+		cout<<"The list is empty"<<endl;
+	}
+	else{
+		Node * pre = NULL;
+		Node * now = head;
+		while(now->next != NULL){
+			pre = now;
+			now = now->next;
+		}
+		pre->next = NULL;
+		delete now;
+	}
+}
+
+void pop_index(int num, Node*& head){
+	if(head==NULL) cout<<"The list is empty"<<endl;
+	else{
+		if(num<=0) cout<<"Out of index"<<endl;
+		else if(num==1){
+			Node * tmp = head;
+			head = head->next;
+			delete tmp;
+		}
+		else{
+			Node * pre = NULL;
+			Node * tmp = head;
+			for(int i=1; i<num; i++){
+				pre = tmp;
+				tmp = tmp->next;
+				if(tmp == NULL){
+					cout<<"Out of index"<<endl;
+					return;
+				}
+			}
+			pre->next = tmp->next;
+			delete tmp;
+		}
+	}
+	
+}
+
+void pop_num(int num, Node*& head){	
+	if(head == NULL) cout<<"The list is empty"<<endl;
+	else{
+		Node * pre = NULL;
+		Node * tmp = head;
+		int index = 0;
+		while(tmp!=NULL){
+			if(tmp->data == num){
+				if(index == 0){
+					head = tmp->next;
+					delete tmp;
+				}
+				else{
+					pre->next = tmp->next;
+					delete tmp;
+				}
+				break;
+			}
+			pre = tmp;
+			tmp = tmp->next;
+			index++;
+		}
+	}
+}
+
+void search(int num, Node*& head){
+	if(head == NULL) cout<<"The list is empty"<<endl;
+	else{
+		Node * tmp = head;
+		while(tmp!=NULL){
+			if(tmp->data==num){
+				cout<<"Number is in the list"<<endl;
+				break;
+			}
+			tmp = tmp->next;
+		}
+	}
+}
+
+void print_list(Node * head){
+	Node * current = head;
+	while(current!=NULL){
+		cout<<current->data<<" ";
+		current = current->next;
+	}
+	cout<<endl;
+}
