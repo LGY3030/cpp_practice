da8fadfed9c25a07ab5d6a26c1e142d23db07d94
LGY3030 <lgy3030@gmail.com>
2021-01-28 14:32:47 +0800

    Leetcode-739, 64, 118

---
+++ Leetcode/118/Pascals_Triangle.cpp
@@ -0,0 +1,37 @@
+//Solution 1
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        if(numRows==0) return{};
+        vector<vector<int>> res;
+        for(int i=0;i<numRows;i++){
+            if(i==0) res.push_back(vector<int>(1,1));
+            else if(i==1) res.push_back(vector<int>(2,1));
+            else{
+                vector<int> vec;
+                vec.push_back(1);
+                for(int j=1;j<i;j++){
+                    int tmp=res[i-1][j-1]+res[i-1][j];
+                    vec.push_back(tmp);
+                }
+                vec.push_back(1);
+                res.push_back(vec);
+            }
+        }
+        return res;   
+    }
+};
+
+
+//Solution 2
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        for(int i=0;i<numRows;i++){
+            res.push_back(vector<int>(i+1,1));
+            for(int j=1;j<i;j++) res[i][j]=res[i-1][j-1]+res[i-1][j];
+        }
+        return res;   
+    }
+};
---
+++ Leetcode/739/Daily_Temperatures.cpp
@@ -0,0 +1,89 @@
+//Solution 1, Time Exceeded
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        if(T.size()<=1) return vector<int>(1);
+        vector<int> vec(T.size());
+        for(int i=0;i<T.size()-1;i++){
+            for(int j=i+1;j<T.size();j++){
+                if(T[j]>T[i]){
+                    vec[i]=j-i;
+                    break;
+                }
+                vec[i]=0;
+            }
+        }
+        return vec;
+    }
+};
+
+//Solution 2, struct
+struct twonum{
+    int tem;
+    int count;
+};
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        if(T.size()<=1) return vector<int>(1);
+        vector<int> vec(T.size());
+        stack<twonum> sta;
+        twonum twon;
+        for(int i=T.size()-1;i>=0;i--){
+            int cnt=0;
+            int flag=0;
+            while(sta.size()!=0){
+                if(T[i]>=sta.top().tem){
+                    cnt+=sta.top().count;
+                    sta.pop();
+                }
+                else{
+                    cnt++;
+                    flag=1;
+                    break;
+                }
+            }
+            twon.tem=T[i];
+            if(flag==0) twon.count=0;
+            else twon.count=cnt;
+            sta.push(twon);
+            vec[i]=twon.count;
+        }
+        return vec;
+    }
+};
+
+
+//Solution 3, pair
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        if(T.size()<=1) return vector<int>(1);
+        vector<int> vec(T.size());
+        stack<pair<int, int>> sta;
+        for(int i=T.size()-1;i>=0;i--){
+            int cnt=0;
+            int flag=0;
+            while(sta.size()!=0){
+                if(T[i]>=sta.top().first){
+                    cnt+=sta.top().second;
+                    sta.pop();
+                }
+                else{
+                    cnt++;
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0){
+                sta.push(make_pair(T[i], 0));
+                vec[i]=0;
+            }
+            else{
+                sta.push(make_pair(T[i], cnt));
+                vec[i]=cnt;
+            }
+        }
+        return vec;
+    }
+};
