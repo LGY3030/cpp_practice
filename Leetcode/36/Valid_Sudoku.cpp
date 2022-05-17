63607d5cefbd1838f23733a9a3609152b3a59961
LGY3030 <lgy3030@gmail.com>
2022-05-16 21:21:34 -0700

    update leetcode: 36

--- Leetcode/36/Valid_Sudoku.cpp
+++ Leetcode/36/Valid_Sudoku.cpp
@@ -1,3 +1,4 @@
+//Solution 1
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
@@ -21,3 +22,49 @@ public:
 };
 
 
+//Solution 2
+class Solution {
+private:
+    bool vertical(vector<vector<char>>& board){
+        for(int i = 0; i < 9; i++){
+            vector<int> check(9, 0);
+            for(int j = 0; j < 9; j++){
+                if(board[j][i] != '.'){
+                    if(check[board[j][i] - '0' - 1] == 1) return false;
+                    check[board[j][i] - '0' - 1] = 1;
+                }
+            }
+        }
+        return true;
+    }
+    bool horizontal(vector<vector<char>>& board){
+        for(int i = 0; i < 9; i++){
+            vector<int> check(9, 0);
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] != '.'){
+                    if(check[board[i][j] - '0' - 1] == 1) return false;
+                    check[board[i][j] - '0' - 1] = 1;
+                }
+            }
+        }
+        return true;
+    }
+    bool rec(vector<vector<char>>& board){
+        unordered_map<int, vector<int>> umap;
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] != '.'){
+                    int idx = i / 3 * 3 + j / 3;
+                    if(umap.find(idx) == umap.end()) umap[idx] = vector<int>(9, 0);
+                    if(umap[idx][board[i][j] - '0' - 1] == 1) return false;
+                    umap[idx][board[i][j] - '0' - 1] = 1;
+                }
+            }
+        }
+        return true;
+    }
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        return vertical(board) && horizontal(board) && rec(board);
+    }
+};
